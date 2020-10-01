75799ada10bb8ab7aa37817e7ee83c36da7eff3d
yitongknows <61811966+yitongknows@users.noreply.github.com>
2020-10-01 15:43:37 -0400

    adding email field

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required, Email
 
 app = Flask(__name__)
 # configure an encryption key
@@ -16,16 +16,22 @@ moment = Moment(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     name = None
+    email = None
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+
+        email = form.email.data
+        if 'utoronto' not in email.lower():
+            flash('Looks like you have changed your email!')
+        session['email'] = form.email.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form,
                            name=session.get('name'),
-                           current_time=datetime.utcnow())
+                           email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
@@ -41,6 +47,7 @@ def internal_server_error(e):
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
+    email = StringField('What is your UofT Email address?', validators=[Email()])
     submit = SubmitField('Submit')
 
 if __name__ == '__main__':
